db5ee276618c5b2dc57d0d67e2834534cb7cc817
ja2006203966 <60353806+ja2006203966@users.noreply.github.com>
2020-11-09 22:04:06 +0800

    Add files via upload

--- N_subjettiness.py
+++ N_subjettiness.py
@@ -13,14 +13,11 @@ import pathlib
 
 path = pathlib.Path.cwd()
 
+
+
 def flatten(event): # for single event in order to reshape data
-#     fp = np.concatenate((np.expand_dims(event[0], axis=-1),
-#                          np.expand_dims(event[1], axis=-1),
-#                          np.expand_dims(event[2], axis=-1),
-#                          np.expand_dims(event[3], axis=-1)), axis=-1)
     z = np.zeros((event.shape[0],1))
     fp = np.append(event, z, axis=-1)
-#     fp = event
     fp = fp.transpose((1,0))
     fp = np.core.records.fromarrays( [fp[:][0],fp[:][1],fp[:][2],fp[:][3]], names= 'pT, eta, phi, mass' , formats = 'f8, f8, f8,f8')
 
@@ -36,7 +33,7 @@ def jet_clustering(event, R0, p = -1):
     return jets
 def t0(jet):
     return sum(p.pt * CalcDeltaR(p, jet) for p in jet.constituents())
-def tn(jet, n, beta = 2): #t1 t2 t3 t21 t32
+def tn(jet, n, beta = 1, R=1): #t1 t2 t3 t21 t32
     assert n >= 0
     if n == 0:
         return t0(jet)
@@ -45,15 +42,13 @@ def tn(jet, n, beta = 2): #t1 t2 t3 t21 t32
         return -1
     subjets = pyjet.cluster(particles, R=1.0, p=1/beta).exclusive_jets(n) #Common choices include using exclusive kt axes or using “minimal” axes
                                                                         #generalised-kt algorithm with p = 1/2.1
-#     subjets_array = [subjet.constituents_array() for subjet in subjets]
     wta_axes = subjets
     wta_axes = np.array(wta_axes)
     if len(wta_axes)<n:
         return -1
     
-#     wta_axes = [a[np.argmax(a['pT'])] for a in subjets_array]
-#     wta_axes = np.array(wta_axes, dtype=subjets_array[0].dtype)
-    return np.sum((particles['pT']*(CalcDeltaRA(particles, wta_axes))**beta).min(axis=0)) / t0(jet)
+    return np.sum((particles['pT']*(CalcDeltaRA(particles, wta_axes))**beta).min(axis=0)) / (jet.pt*(R**beta))
+
 def CalcDeltaRArray(p, a):
     dEta = p['eta'] - \
         a['eta'].repeat(p.shape[0]).reshape(a.shape[0], p.shape[0])
@@ -72,8 +67,6 @@ def CalcDeltaRA(p, j2): ##FOR Taylor's code
         pa = np.transpose(pa, [1,0])
         dEP =pa - ep.repeat(p.shape[0]).reshape(ep.shape[0], p.shape[0])
         dEP[:][1] = np.abs(dEP[:][1])
-#         dEta = p['eta'] - ep[0].repeat(p.shape[0]).reshape(ep.shape[0], p.shape[0])
-#         dPhi = np.abs(p['phi'] - a['phi'].repeat(p.shape[0]).reshape(a.shape[0], p.shape[0]))
         mask = dEP[:][1] > np.pi
         dEP[:][1][mask] *= -1
         dEP[:][1][mask] += 2 * np.pi
@@ -95,15 +88,17 @@ def CalcDeltaR(j1, j2):
 
     return dR
 
-def T21(event):
+def T21(event, beta=1):
     jet = jet_clustering(event, 1)[0]
-    t1 = tn(jet, n=1)
-    t2 = tn(jet, n=2)
+    t1 = tn(jet, n=1, beta = beta)
+    t2 = tn(jet, n=2, beta = beta)
     #         t3 = tn(found_jet, n=3)
     t21 = t2 / t1 if t1 > 0.0 else 0.0
+    if t21<0:
+        t21 = 0
     return t21
 
-def generate_NSJ(file_path, NUM_Ev= None): # NUM_Ev= number of events you choose if = None => all the events
+def generate_NSJ(file_path, NUM_Ev= None, beta=1, target = None):
     NSJ = []
     df = pd.read_hdf(file_path, "features")
     y = pd.read_hdf(file_path, "targets")
@@ -111,8 +106,9 @@ def generate_NSJ(file_path, NUM_Ev= None): # NUM_Ev= number of events you choose
     if NUM_Ev == None:
         NUM_Ev = len(df.iloc[:])
     for i in tqdm(range(NUM_Ev)):
-#         if y.iloc[i].values[0] == 1:
-#             continue
-        event = df.iloc[i].values[0]        
-        NSJ.append(T21(event))
+        if target != None:
+            if y.iloc[i].values[0] == 1-target:
+                continue
+        event = df.iloc[i].values[0]
+        NSJ.append(T21(event, beta=beta))
     return(NSJ)
